8a6dc5f49b82e3b2ebd6d154cc2609a0e7a8eb3d
Chiffa <artemy040404@gmail.com>
2026-03-12 14:03:32 +0300

    feat: add blog post list page

--- init.lisp
+++ init.lisp
@@ -6,7 +6,15 @@
 (defvar *server* nil
   "Running Hunchentoot acceptor")
 
-(defconstant +cwd+ (uiop/os:getcwd))
+(defvar *cwd*
+  (uiop/pathname:ensure-pathname
+   (find-if #'identity (list (uiop/os:getenvp "BLOG_POST_PATH") (uiop/os:getcwd)))))
+
+(defvar *search-pattern* 
+  (concatenate 'string 
+               (uiop/filesystem:native-namestring *cwd*)
+               "*.md")
+  "Search pattern for markdown files")
 
 (defun start-server (&key (port 8080))
   "Start the web server on PORT (default 8080).
@@ -28,6 +36,13 @@ If a server is already running it is stopped first."
         (format t "~&; Server stopped.~%"))
       (format t "~&; No server is running.~%")))
 
+(defun nav-bar ()
+  (spinneret:with-html
+     (:nav
+      (:div :style "display: flex; flex-direction: row; justify-content: space-evenly" 
+       (:div (:a :href "/home" "home")) 
+       (:div (:a :href "/post_list" "Posts")))
+    )))
 
 (defmacro with-page ((&key title) &body body)
   `(spinneret:with-html-string
@@ -43,18 +58,15 @@ If a server is already running it is stopped first."
      (:html
       (:head
        (:title ,title))
-      (:body (:raw ,@body)))))
+      (:body
+       (nav-bar)
+       (:raw ,@body)))))
 (defun home-page ()
   (with-page 
     (:title "Home page")
     (:header
-     (:nav
-      (:div
-       (:a :href "/home" "Home page"))
-      (:div
-       (:a :href "/test" "test"))))
-    ))
-
+     (nav-bar)
+    )))
 
 (hunchentoot:define-easy-handler (test :uri "/test") ()
   (print "Hitting /test")
@@ -89,21 +101,40 @@ If a server is already running it is stopped first."
      (sync-dispatch-table)
      ',uri))
 
+(defun to-url-list (file-name-list)
+  (mapcar 
+   (lambda (filename) 
+     (spinneret:with-html (:a :href (concatenate 'string "/md/" filename) filename)))
+   file-name-list))
+
+(define-route "/post_list"
+  (let* ((file-list (directory *search-pattern*))
+         (file-names (mapcar 'pathname-name file-list)))
+    
+    
+    (setf (hunchentoot:content-type*) "text/html")
+    (with-page 
+        (:title "Post list")
+      (to-url-list file-names)))
+  )
+
 (define-route "/file/"
   (let* ((uri (hunchentoot:request-uri*))
          (filename (subseq uri (length "/file/"))))
     (setf (hunchentoot:content-type*) "text/plain")
-    (uiop:read-file-string (merge-pathnames filename +cwd+))))
+    (uiop:read-file-string (merge-pathnames filename *cwd*))))
 
-(defun markdown-from-file (file) 
+(defun markdown-from-file (file title) 
   (with-page-raw 
-    (:title "test")
-    (nth-value 1 (cl-markdown:markdown (uiop:read-file-string file) :format :html :stream nil))
+    (:title title)
+    (nth-value 
+     1 
+     (cl-markdown:markdown (uiop:read-file-string file) :format :html :stream nil))
    ))
 
 (define-route "/md/"
   (let* ((uri (hunchentoot:request-uri*))
          (filename (subseq uri (length "/md/")))
-         (file-path (merge-pathnames (concatenate 'string filename ".md") +cwd+)))
+         (file-path (merge-pathnames (concatenate 'string filename ".md") *cwd*)))
     (setf (hunchentoot:content-type*) "text/html")
-    (markdown-from-file file-path)))
+    (markdown-from-file file-path filename)))
